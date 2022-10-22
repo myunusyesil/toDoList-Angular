13210572eab1165e8f58bb23e1ebbc5c49701117
myunusyesil <mehmetyunus@windowslive.com>
2022-10-22 23:05:18 +0300

    almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component,  ViewChild} from '@angular/core';
+import {ElementRef, Renderer2} from '@angular/core';
+import { ListObjectModel } from './list-object.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
+  @ViewChild('input') el!: ElementRef;
+  @ViewChild('parent') par!: ElementRef;
+  @ViewChild('child') chil!: ElementRef;
+  
+   listItem: ListObjectModel = {
+    id: 0,
+    text: "",
+    isComp: false,
+   };
+
+   listArr:Array<any> = []; 
+
   title = 'toDoList';
+  inputText : string = '';
+  id: number = 0;
+  isCompleted: boolean = false;
+
+
+  constructor(private rd: Renderer2) {}
+
+  onClickDel(event: any, child: any) {
+    // debugger;
+    // let item = event.target.parentElement.parentElement;
+    // console.log(item)
+    // item.style.display = 'none';
+    this.chil.nativeElement = child;
+    console.log (this.par,this.chil);
+    this.rd.removeChild(this.par.nativeElement, this.chil.nativeElement);
+  
+  }
+
+  onEnter(event: any, text: string) {
+
+    event.preventDefault();
+    const d = new Date;
+    this.id += d.getDate(); 
+    this.inputText = text;
+    this.listItem = {id : this.id, text: this.inputText, isComp: this.isCompleted};
+    // console.log(this.listItem);
+    this.listArr.push(this.listItem);
+    
+    // console.log(this.el.nativeElement)
+    this.el.nativeElement.value = "";
+    // console.log(this.listArr);
+ 
+  }
+
+  onCheckBoxClick(event: any, listItem: {id: number, text: string, isComp: boolean}) {
+ 
+    console.log(listItem);
+    if ( !listItem.isComp) {
+    event.target.parentElement.classList.add("checked");
+    }
+    else {
+      event.target.parentElement.classList.remove("checked");
+    }
+    this.isCompleted = !this.isCompleted;
+    listItem.isComp = this.isCompleted;
+    // console.log(this.listArr)
+
+  }
+  
+
 }
---
+++ src/app/list-object.model.ts
@@ -0,0 +1,5 @@
+export class ListObjectModel{
+
+    constructor (id: number, text: string, isComp: boolean) {
+    }
+}
