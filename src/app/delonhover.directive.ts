bbbc67264c1aa7bb4ef0f6f8e8e724a8ed3feca9
myunusyesil <mehmetyunus@windowslive.com>
2022-10-24 14:25:18 +0300

    hover-eff

--- src/app/delonhover.directive.ts
+++ src/app/delonhover.directive.ts
@@ -11,10 +11,12 @@ export class DelonhoverDirective implements OnInit{
 
   @HostListener('mouseenter') onMouseEnter (eventData: Event) {
     this.el.nativeElement.children[2].children[0].classList.add('hover')
+    this.el.nativeElement.classList.add('itemBackground')
   }
 
   @HostListener('mouseleave') onMouseLeave (eventData: Event) {
     this.el.nativeElement.children[2].children[0].classList.remove('hover')
+    this.el.nativeElement.classList.remove('itemBackground')
   }
   constructor(private el: ElementRef, private rd: Renderer2) {
  
